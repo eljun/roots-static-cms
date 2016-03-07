d8b3142bc27bffdb40a2e88bffa3e58090131620
eljun <el.junsan@yahoo.com>
2016-03-07 17:47:36 +0800

    Adding screen orientation using manifest

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,7 +1,1 @@
-var show = function() {
-     console.log("Orientation type is " + screen.orientation.type);
-     console.log("Orientation angle is " + screen.orientation.angle);
-  }
-
-  screen.orientation.addEventListener("change", show);
-  window.onload = show;
+screen.orientation.lock('landscape');
